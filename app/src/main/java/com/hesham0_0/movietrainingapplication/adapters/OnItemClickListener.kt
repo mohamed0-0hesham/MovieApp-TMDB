04181b33856c2c6e050ab7e4861c435185dd840b
Mohamed Hesham <m.hesham1851999@gmail.com>
2026-01-09 21:44:56 +0200

    Removed the extra files
    - Removed the extra files after reposition the files

--- app/src/main/java/com/hesham0_0/movietrainingapplication/adapters/OnItemClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.hesham0_0.movietrainingapplication.adapters
-
-import com.hesham0_0.movietrainingapplication.models.MovieItem
-
-
-interface OnItemClickListener {
-    fun onItemClick(position: Int, movieItem: MovieItem)
-}
